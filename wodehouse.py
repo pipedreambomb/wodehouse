817c683d42d7d535b08c715b7470de2bc3496ed1
George Nixon <g@georgenixon.co.uk>
2026-06-16 21:53:44 +0100

    Initial commit

---
+++ wodehouse.py
@@ -0,0 +1,20 @@
+from toolz.curried import pipe, pluck, map, first
+import json
+from requests import request
+
+response = request('get', 'https://gutendex.com/books?search=wodehouse')
+
+# TODO this is only the first page! we need all the Wodehouse.
+books = json.loads(response.content)
+
+downloadUrls = pipe(
+    books['results'],
+    pluck('formats'),
+    map(lambda book: (url for format, url in book.items() if format.startswith('text/plain'))),
+    # take one, as sometimes there's more than one plain text format per book, e.g. UTF-8 and ASCII
+    map(first),
+    # flatten
+    list
+)
+
+print(downloadUrls)
